145b1a3b38a755e57bc427b68528ff89dfc179f1
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-07-15 13:27:15 +0000

    [Add]customer controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,29 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
+
     resources :items, only: [:index, :show]
+
+    resource :customers do
+      get '/my_page' => 'customers#show'
+      get '/information/edit' => 'customers#edit'
+      patch '/information' => 'customers#update'
+      get '/unsubscribe' => 'customers#unsubscribe'
+      patch '/withdraw' => 'customers#withdraw'
+    end
+
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      delete :delete_all, on: :collection
+    end
+
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post '/confirm' => 'orders#confirm'
+        get '/complete' => 'orders#complete'
+      end
+    end
+
+    resources :addresses, except: [:new, :show]
   end
 
   devise_for :customers, skip: [:passwords], controllers: {
