ffa0ed773fc0220c7736ae38aa7793cc3b0a480c
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-07-15 09:32:59 +0000

    [Add]customer controller追加(途中)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+  end
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
