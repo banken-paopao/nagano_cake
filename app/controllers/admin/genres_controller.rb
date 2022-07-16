3bc5edb23da7a0697455b1ed4cc70383cf79c5ea
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-16 04:26:31 +0000

    [Add]genreの編集ページの追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,8 +6,18 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
