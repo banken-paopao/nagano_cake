2349deab75f70c4360a676d007b7aa32ed2af329
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-07-18 08:49:36 +0000

    [Add]商品がカートに存在するときの処理

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -19,7 +19,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    #カート内にある同じ商品をすべて取得
+    now_cart = current_customer.cart_items.where(item_id: params[:cart_item][:item_id])
     cart_item = current_customer.cart_items.new(cart_item_params)
+    unless now_cart.blank?
+      #存在した時は購入個数を足して一つにまとめる
+      now_cart.each do |item|
+        cart_item.amount += item.amount
+        item.destroy
+      end
+    end
     if cart_item.save
       redirect_to cart_items_path
     else
