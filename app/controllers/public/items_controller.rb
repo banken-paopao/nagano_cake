65fde1969ab105b7a4fa42150dd2d381771b438f
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-19 13:34:02 +0000

    [Update]customer側の修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -10,10 +10,22 @@ class Public::ItemsController < ApplicationController
     @cart_item = CartItem.new
   end
 
-  def genre_search
-    @genres = Genre.all
-    items = Item.genre_search(params[:genre_id])
-    @items = items.all
-    @genre_name = Genre.find(params[:genre_id]).name
+  def item_search
+    @model = params[:model]
+    if @model == "item"
+      unless params[:content] == ""
+        @genres = Genre.all
+        @search_name = params[:content]
+        @search_result = Item.search_for(@model, @search_name)
+      else
+        flash[:danger] = "商品名を入力してください"
+        redirect_to items_path
+      end
+    else
+      @genres = Genre.all
+      search_genre = params[:genre_id]
+      @search_result = Item.search_for(@model, search_genre)
+      @search_name = Genre.find(search_genre).name
+    end
   end
 end
