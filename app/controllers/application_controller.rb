e25d4b8800ef566b4459f2b991afd6838ceafb42
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-20 08:34:12 +0000

    [Update]ログアウト後の遷移先の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin then
+      new_admin_session_path
+    when :customer then
+      root_path
+    end
+  end
 end
