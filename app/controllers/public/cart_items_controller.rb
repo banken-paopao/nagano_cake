9d09fc723617bc7918f1e3b444c4bffad21fdf37
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-07-21 08:56:33 +0000

    [Update]cart-items-ajax

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,20 +7,18 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    cart_item = CartItem.find(params[:id])
-    cart_item.update(cart_item_params)
-    redirect_to request.referer
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
   end
 
   def destroy
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    redirect_to cart_items_path
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
   end
 
   def destroy_all
     current_customer.cart_items.destroy_all
-    redirect_to request.referer
+
   end
 
   def create
